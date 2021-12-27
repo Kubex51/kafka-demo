cc34063015feb9723e77cbd690b83a9304218ebc
Kubex51 <j.owczarski01@gmail.com>
2021-12-27 16:26:07 +0100

    Commit of ES & TT project

---
+++ kafka-es-consumer/src/main/java/ElasticSearchConsumer.java
@@ -0,0 +1,111 @@
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ElasticSearchConsumer {
+    public static RestHighLevelClient createClient() {
+        String hostname = "kafka-course-testing-4432511764.eu-central-1.bonsaisearch.net";
+        String username = "7f3grqy8jj";
+        String password = "3yuym5gmo7";
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+    public static KafkaConsumer<String, String> createConsumer(String topic){
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elasticsearch";
+
+        //create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+        return consumer;
+    }
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String, String> consumer = createConsumer("TwitterSearch_topic");
+
+        int numfrommesstoread = 5;
+        boolean keeponreading = true;
+        int numread = 0;
+
+        while(keeponreading){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String,String> record: records){
+                numread += 1;
+//                logger.info("Key: " + record.key() + "\n" +
+//                        "Value: " + record.value() + "\n" +
+//                        "Partition: " + record.partition() + "\n"+
+//                        "Offset: " + record.offset() + "\n");
+                String jsonString = record.value();
+                IndexRequest indexRequest = new IndexRequest(
+                        "twitter",
+                        "tweets"
+                ).source(jsonString, XContentType.JSON);
+
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                String id = indexResponse.getId();
+                logger.info(id);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(numfrommesstoread < numread){
+                    keeponreading = false;
+                    break;
+                }
+            }
+
+        }
+        logger.info("Exiting the application");
+
+        client.close();
+    }
+}
---
+++ kafka-tw-producer/src/main/java/TwitterProducer.java
@@ -0,0 +1,130 @@
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+    public static void main(String[] args) throws InterruptedException {
+        //get twitter data
+        //setup blocking queues
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+        
+        //declare host
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        List<String> terms = Lists.newArrayList("kafka", "java", "python", "linux", "bitcoin");
+        hosebirdEndpoint.trackTerms(terms);
+
+        Authentication hosebirdAuth = new OAuth1(
+                "s3vaERZbLQzyv5Y3wkMGKvy7B",
+                "6lbFsfWPI2Ql1WiqxV4MnCrtR2MWdWUwdDe6slYSu9ktNErKec",
+                "4882187681-JZVMAlMkHCXgZbrdTGS5m6F72syVTsIs9BaAKTq",
+                "kQ0sMB72iec4Jhr0gLcw8apKBUdgVWpMffdZzKSBu2AaS");
+        
+        //Client
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+        Client hosebirdClient = builder.build();
+
+        
+        // producer variables
+        String bootstrapServer = "127.0.0.1:9092";
+        String topic = "TwitterSearch_topic";
+
+        //create Producer properties
+        Properties producerProperties = new Properties();
+        producerProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        producerProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //additional properties for ensuring producer safety
+        producerProperties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        producerProperties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        producerProperties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        producerProperties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        //compression setting
+        producerProperties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4");
+        producerProperties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1000");
+        producerProperties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        //create the producer
+        KafkaProducer<String, String> TwitterProducer = new KafkaProducer<String, String>(producerProperties);
+
+        //connect client
+        hosebirdClient.connect();
+
+        int PostsToDownload = 100;
+        int PostDownloaded = 0;
+
+        //create logger
+
+        Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+
+        //ShutdownHook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutting down application...");
+            logger.info("Stopping client...");
+            hosebirdClient.stop();
+            logger.info("Closing producer...");
+            TwitterProducer.flush();
+            TwitterProducer.close();
+            logger.info("Application closed");
+        }));
+
+        while(!hosebirdClient.isDone() && PostsToDownload > 0) {
+            //create producer record
+            String msg = null;
+            try{
+                 msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+                hosebirdClient.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(
+                        topic,
+                        Integer.toString(PostDownloaded),
+                        msg
+                );
+                PostDownloaded += 1;
+                PostsToDownload -= 1;
+                logger.info("Posts to Download: " + Integer.toString(PostsToDownload));
+                TwitterProducer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if (e == null) {
+                            logger.info("Successfully pulled" + "\n" +
+                                    "Topic: " + recordMetadata.topic() + "\n" +
+                                    "Partition: " + recordMetadata.partition() + "\n" +
+                                    "Offset: " + recordMetadata.offset() + "\n" +
+                                    "Timestamp: " + recordMetadata.timestamp() + "\n");
+                        } else {
+                            logger.error("Error while producing" + e);
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
